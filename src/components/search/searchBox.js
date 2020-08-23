5b7c551f401e4d58343d304c65988e74d9927b21
vishwajeet singh <vsrnitp@gmail.com>
2020-08-23 12:35:10 +0530

    search done

--- src/components/search/searchBox.js
+++ src/components/search/searchBox.js
@@ -1,8 +1,107 @@
 import React, { Component } from 'react';
 import { Button, View, Text,ScrollView,StyleSheet,Image, TouchableOpacity,TextInput, Alert} from 'react-native';
+import axios from 'axios';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+const URL = `https://villcosmos.vsrnitp.repl.co/api/fastFoodSearch/search`; // API for fast food searching
+const URL1 = `https://villcosmos.vsrnitp.repl.co/api/vegetableSearch/search`; //API for vegetable searching
+
 
 class MasterSearch extends Component{
+
+    state = {
+        fastFoodSearchData : '',
+        vegetableSearchData:'',
+        hint:'please enter search string....'
+    }
+    
+   
+    handleSearch = (text) => {
+     var regex = /^[0-9a-zA-Z]+$/;
+      if(text.match(regex)){
+        const request = axios.post(`${URL}`,{"searchString":text}).then(response => {
+            const searchData = [];
+           
+            for(let key in response.data){
+                searchData.push({
+                    ...response.data[key],
+                    id:key
+                })  
+            }
+            const request1 = axios.post(`${URL1}`,{"searchString":text}).then(response1 => {
+                const searchData1 = [];
+
+                for(let key in response1.data){
+                    searchData1.push({
+                        ...response1.data[key],
+                        id:key
+                    })  
+                }
+                if(searchData && text!=='')this.setState({"fastFoodSearchData":searchData});
+                if(searchData1 && text!=='')this.setState({"vegetableSearchData":searchData1});
+                console.log(text)
+            })
+       }).catch(e=>{
+           console.log(e);
+       })
+    }
+}
+
+
+renderFastFoodSearchData = () => (
+    this.state.fastFoodSearchData ? 
+        this.state.fastFoodSearchData.map((item)=>(
+            <View key = {item.id}>
+            <TouchableOpacity delayPressIn={0}>
+            <View style={{flex:1,flexDirection:'column'}}>
+                <Text style={{fontWeight:'bold', fontSize:20,paddingBottom:5}}>{item.productName}</Text>
+                <Text style={{color:'grey',fontSize:15,paddingBottom:5}}>{item.productDescription}</Text>
+                <View style={{flex:1,flexDirection:'row'}}>
+                <Text style={{fontWeight:'bold',color:'teal',fontSize:18}}>Price - {item.productPrice}/Unit</Text>
+                <Text style={{fontWeight:'bold',color:'grey',paddingLeft:'40%'}}>Fast Food</Text>
+                </View>
+                </View>
+                <View
+            style={{
+            borderBottomColor: 'teal',
+            borderBottomWidth: 1,
+            padding:2
+                }}
+                />
+                </TouchableOpacity>
+            </View>
+        ))
+        :
+        <Text>{this.state.hint}</Text>
+)
+     
+                
+
+renderVegetableSearchData = () => (
+    this.state.vegetableSearchData ? 
+        this.state.vegetableSearchData.map((item)=>(
+            <View key = {item.id}>
+            <TouchableOpacity delayPressIn={0}>
+            <View style={{flex:1,flexDirection:'column'}}>
+                <Text style={{fontWeight:'bold', fontSize:20,paddingBottom:5}}>{item.productName}</Text>
+                <Text style={{color:'grey',fontSize:15,paddingBottom:5}}>{item.productDescription}</Text>
+                <View style={{flex:1,flexDirection:'row'}}>
+                <Text style={{fontWeight:'bold',color:'teal',fontSize:18}}>Price - {item.productPrice}/Unit</Text>
+                <Text style={{fontWeight:'bold',color:'grey',paddingLeft:'40%'}}>Vegetable</Text>
+                </View>
+                </View>
+                <View
+            style={{
+            borderBottomColor: 'teal',
+            borderBottomWidth: 1,
+            padding:2
+                }}
+                />
+                </TouchableOpacity>
+            </View>
+        ))
+        :
+        <Text></Text>
+)
 
     render(){
         return(
@@ -12,7 +111,9 @@ class MasterSearch extends Component{
             <TextInput style={{color:'black',height:50,width:'95%',fontSize:14,backgroundColor:'#F0FFF0',borderRadius:5,margin:10}}
             placeholder="search for anything...(fruits,vegetables etc.)"
             placeholderTextColor="teal"
+            onChangeText={this.handleSearch}
             />
+            {/*
             <TouchableOpacity delayPressIn={0}>
             <View style={{backgroundColor:'teal',paddingVertical:12,paddingHorizontal:25,borderRadius:25}}>
             <Text style={{color:'white',fontSize:20}}>Search{'  '}
@@ -20,7 +121,13 @@ class MasterSearch extends Component{
             </Text>
             </View>
             </TouchableOpacity>
+            */}
             </View>
+            <View style={{padding:15}}>
+                {this.renderFastFoodSearchData()}
+                {this.renderVegetableSearchData()}
+            </View>
+           
             </ScrollView>
         )
     }
