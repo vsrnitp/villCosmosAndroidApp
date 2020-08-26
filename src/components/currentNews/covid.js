04f127d6cb5cc2b1878186ca9d57cfb74aa73b14
vishwajeet singh <vsrnitp@gmail.com>
2020-08-26 10:21:55 +0530

    covid tracker improved

--- src/components/currentNews/covid.js
+++ src/components/currentNews/covid.js
@@ -4,7 +4,8 @@ import {
     StyleSheet,
     View,
     Text,
-    TouchableOpacity
+    TouchableOpacity,
+    ActivityIndicator
   } from 'react-native';
   import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
@@ -14,7 +15,8 @@ import {
         ActiveCases:'',
         Discharged:'',
         Death:'',
-        TotalCases:''
+        TotalCases:'',
+        DataReceived:false,
     }
 
     getCovidData = () => {
@@ -22,7 +24,7 @@ import {
                             .then((response) => {
                                 var data = response.data;
                                const values = Object.values(data);
-                                this.setState({"ActiveCases":values[2],"Discharged":values[4],"Death":values[5],"TotalCases":values[3]})
+                                this.setState({"ActiveCases":values[2],"Discharged":values[4],"Death":values[5],"TotalCases":values[3],"DataReceived":true})
                             })
                             .catch(e => console.log(e));
     }
@@ -43,11 +45,23 @@ import {
               <MaterialCommunityIcons name="virus" size={50} color={'teal'} />
               <Text style={{fontWeight:'bold',color:'teal',fontSize:25,padding:10}}>Covid Data Here...!</Text>
 
-              <Text style={{fontWeight:'bold',color:'yellow',fontSize:20,padding:7}}>Current Active Cases - {this.state.ActiveCases}</Text>
-              <Text style={{fontWeight:'bold',color:'darkgreen',fontSize:20,padding:7}}>Recovered Patients - {this.state.Discharged}</Text>
-              <Text style={{fontWeight:'bold',color:'teal',fontSize:20,padding:7}}>Total Covid Cases (India) - {this.state.TotalCases}</Text>
-              <Text style={{fontWeight:'bold',color:'red',fontSize:20,padding:7}}>Total Death Count - {this.state.Death}</Text>
+              {this.state.DataReceived &&
+              <View>
+              <Text style={{fontWeight:'bold',color:'yellow',fontSize:20,padding:7,textAlign:'center'}}>Current Active Cases - {this.state.ActiveCases}</Text>
+              <Text style={{fontWeight:'bold',color:'darkgreen',fontSize:20,padding:7,textAlign:'center'}}>Recovered Patients - {this.state.Discharged}</Text>
+              <Text style={{fontWeight:'bold',color:'teal',fontSize:20,padding:7,textAlign:'center'}}>Total Covid Cases (India) - {this.state.TotalCases}</Text>
+              <Text style={{fontWeight:'bold',color:'red',fontSize:20,padding:7,textAlign:'center'}}>Total Death Count - {this.state.Death}</Text>
+              </View>
+              }
 
+              {!this.state.DataReceived &&
+                <ActivityIndicator
+                style={{justifyContent:'center',padding:25}}
+                color = '#bc2b78'
+                 size = "large"
+                />
+              }
+              
               <TouchableOpacity delayPressIn={0} onPress={this.getCovidData}>
               <View style={{backgroundColor:'teal',paddingVertical:12,paddingHorizontal:25,borderRadius:25}}>
                   <Text style={{color:'white',fontSize:18}}>Refresh Data!</Text>
